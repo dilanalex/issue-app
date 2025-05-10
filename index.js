f2312c9702d1d322e2a786831adaa27a2572f85c
Dilan Silva <dil6000@gmail.com>
2025-05-10 11:03:35 +0530

    Issue revision implementation has added

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const bodyParser = require('koa-bodyparser');
 const config = require('./config');
 const router = require('./lib/routes');
 const sequelize = require('./lib/models/connection');
-require('./lib/models/user');
+
+//require('./lib/models/user');
+require('./lib/models/issueRevision');
 
 const app = new Koa();
 
@@ -17,3 +19,12 @@ app.use(router.allowedMethods());
 app.listen(config.port);
 console.log('Listening on http://localhost:%s/', config.port);
 
+/*sequelize.sync({ alter: true })
+  .then(() => {
+    console.log('Database synchronized (with alter)');
+    app.listen(config.port);
+    console.log('Listening on http://localhost:%s/', config.port);
+  })
+  .catch((err) => {
+    console.error('Error synchronizing database:', err);
+  });*/
